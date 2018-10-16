0d718d2ec3300ae1e51a5a0c372779b1d9e544b5
tae <tae@easypost.com>
2018-10-16 08:19:38 -0700

    Hook input fields to action creator methods by accessing DOM

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Generate a id
+function generateId () {
+  return Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36);
+}
+
 // Library Code
 function createStore (reducer) {
   // The store should have four parts
@@ -38,21 +43,21 @@ const ADD_TODO = 'ADD_TODO',
   REMOVE_GOAL = 'REMOVE_GOAL'
 
 // ACTION CREATORS
-function addTodoAction(todo) {
+function addToDoAction(todo) {
   return {
     type: ADD_TODO,
     todo
   }
 }
 
-function removeTodoAction(id) {
+function removeToDoAction(id) {
   return {
     type: REMOVE_TODO,
     id,
   }
 }
 
-function toggleTodoAction(id) {
+function toggleToDoAction(id) {
   return {
     type: TOGGLE_TODO,
     id,
@@ -80,7 +85,7 @@ function mealCreator(id) {
   }
 }
 
-function todos (state = [], action) {
+function toDos (state = [], action) {
   switch (action.type) {
     case ADD_TODO:
       return state.concat([action.todo])
@@ -106,7 +111,7 @@ function goals (state = [], action) {
 
 function app (state = {}, action) {
   return {
-    todos: todos(state.todos, action),
+    todos: toDos(state.todos, action),
     goals: goals(state.goals, action)
   }
 }
@@ -115,7 +120,7 @@ function app (state = {}, action) {
 const store = createStore(app)
  
 store.subscribe(() => {
-  console.log(`The new state is: ${store.getState()}`)
+  console.log(`The new state is: `, store.getState())
 })
 
 // const unsubscribe = store.subscribe(() => {
@@ -125,47 +130,75 @@ store.subscribe(() => {
 // unsubscribe()
 
 
-//  ACTIONS
+// //  ACTIONS - DISPATCH INVOCATIONS
+// store.dispatch(addTodoAction({
+//   id: 0,
+//   name: 'Walk the dog',
+//   complete: false,
+//   }))
 
-store.dispatch(addTodoAction({
-  id: 0,
-  name: 'Walk the dog',
-  complete: false,
+// store.dispatch(addTodoAction({
+//   id: 1,
+//   name: 'Wash the car',
+//   complete: false,
+// }))
+
+// store.dispatch({
+//   type: ADD_GOAL,
+//   goal: {
+//     id: 3,
+//     name: 'TEST',
+//     complete: true,
+//   }
+// })
+
+// store.dispatch(addTodoAction({
+//   id: 2,
+//   name: 'Go to the gym',
+//   complete: true,
+// }))
+
+// store.dispatch(removeTodoAction(1))
+
+// store.dispatch(toggleTodoAction(0))
+
+// store.dispatch(addGoalAction({
+//   id: 0,
+//   name: 'Learn Redux'
+// }))
+
+// store.dispatch(addGoalAction({
+//   id: 1,
+//   name: 'Lose 20 pounds'
+// }))
+
+// store.dispatch(removeGoalAction(0))
+
+function addToDo () {
+  const input = document.getElementById('to-do')
+  const name = input.value
+  input.value = ''
+
+  store.dispatch(addToDoAction({
+    name,
+    complete: false,
+    id: generateId()
   }))
+}
 
-store.dispatch(addTodoAction({
-  id: 1,
-  name: 'Wash the car',
-  complete: false,
-}))
+function addGoal () {
+  const input = document.getElementById('goal')
+  const name = input.value
+  input.value = ''
 
-store.dispatch({
-  type: ADD_GOAL,
-  goal: {
-    id: 3,
-    name: 'TEST',
-    complete: true,
-  }
-})
+  store.dispatch(addGoalAction({
+    name,
+    id: generateId()
+  }))
+}
 
-store.dispatch(addTodoAction({
-  id: 2,
-  name: 'Go to the gym',
-  complete: true,
-}))
+document.getElementById('toDoBtn')
+  .addEventListener('click', addToDo);
 
-store.dispatch(removeTodoAction(1))
-
-store.dispatch(toggleTodoAction(0))
-
-store.dispatch(addGoalAction({
-  id: 0,
-  name: 'Learn Redux'
-}))
-
-store.dispatch(addGoalAction({
-  id: 1,
-  name: 'Lose 20 pounds'
-}))
-
-store.dispatch(removeGoalAction(0))
+document.getElementById('goalBtn')
+  .addEventListener('click', addGoal);
