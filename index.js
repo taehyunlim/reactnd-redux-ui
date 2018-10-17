203291ff3f740323d7d41a6dc60b5aeb1c397b46
tae <tae@easypost.com>
2018-10-16 21:05:57 -0700

    Removed dispatch method invocations in comment

--- index.js
+++ index.js
@@ -138,52 +138,6 @@ store.subscribe(() => {
 
 // unsubscribe()
 
-
-// //  ACTIONS - DISPATCH INVOCATIONS
-// store.dispatch(addTodoAction({
-//   id: 0,
-//   name: 'Walk the dog',
-//   complete: false,
-//   }))
-
-// store.dispatch(addTodoAction({
-//   id: 1,
-//   name: 'Wash the car',
-//   complete: false,
-// }))
-
-// store.dispatch({
-//   type: ADD_GOAL,
-//   goal: {
-//     id: 3,
-//     name: 'TEST',
-//     complete: true,
-//   }
-// })
-
-// store.dispatch(addTodoAction({
-//   id: 2,
-//   name: 'Go to the gym',
-//   complete: true,
-// }))
-
-// store.dispatch(removeTodoAction(1))
-
-// store.dispatch(toggleTodoAction(0))
-
-// store.dispatch(addGoalAction({
-//   id: 0,
-//   name: 'Learn Redux'
-// }))
-
-// store.dispatch(addGoalAction({
-//   id: 1,
-//   name: 'Lose 20 pounds'
-// }))
-
-// store.dispatch(removeGoalAction(0))
-
-
 // DOM Code
 function addToDo () {
   const input = document.getElementById('to-do')
