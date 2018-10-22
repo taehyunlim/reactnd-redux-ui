db3f3d3c067925a9a0ccb4dcef84d8263af0d18b
tae <tae@easypost.com>
2018-10-21 17:53:07 -0700

    Updated UI to React code

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const ADD_TODO = 'ADD_TODO',
   REMOVE_TODO = 'REMOVE_TODO',
   TOGGLE_TODO = 'TOGGLE_TODO',
   ADD_GOAL = 'ADD_GOAL',
-  REMOVE_GOAL = 'REMOVE_GOAL'
+  REMOVE_GOAL = 'REMOVE_GOAL',
+  RECEIVE_DATA = 'RECEIVE_DATA'
 
 // ACTION CREATORS
 function addToDoAction(todo) {
@@ -56,6 +57,15 @@ function mealCreator(id) {
   }
 }
 
+// New set of actions
+function receiveDataAction (todos, goals) {
+  return {
+    type: RECEIVE_DATA,
+    todos,
+    goals
+  }
+}
+
 function todos (state = [], action) {
   switch (action.type) {
     case ADD_TODO:
@@ -64,6 +74,8 @@ function todos (state = [], action) {
       return state.filter((todo) => todo.id !== action.id)
     case TOGGLE_TODO:
       return state.map((todo) => todo.id !== action.id ? todo : Object.assign({}, todo, { complete : !todo.complete }))
+    case RECEIVE_DATA:
+      return action.todos
     default:
       return state
   }
@@ -75,6 +87,17 @@ function goals (state = [], action) {
       return state.concat([action.goal])
     case REMOVE_GOAL:
       return state.filter((goal) => goal.id !== action.id)
+    case RECEIVE_DATA:
+      return action.goals
+    default:
+      return state
+  }
+}
+
+function loading (state = true, action) {
+  switch(action.type) {
+    case RECEIVE_DATA:
+      return false
     default:
       return state
   }
@@ -121,7 +144,8 @@ const commentAlert = (store) => (next) => (action) => {
 
 const store = Redux.createStore(Redux.combineReducers({
   todos: todos,
-  goals: goals
+  goals: goals,
+  loading: loading
   // Invoke checker function as the second argument to createStore, after applying middleware which allows the function to intercept a dispatched action before it reaches the reducer inside the store
   // Redux.applyMiddleware(...middlewares)
   }), Redux.applyMiddleware(
@@ -130,102 +154,102 @@ const store = Redux.createStore(Redux.combineReducers({
     // commentAlert
   ))
  
-store.subscribe(() => {
-  console.log(`The new state is: `, store.getState())
+// store.subscribe(() => {
+//   console.log(`The new state is: `, store.getState())
 
-  // Clear DOM
-  document.getElementById('goals').innerHTML = ''
-  document.getElementById('to-dos').innerHTML = ''
+//   // Clear DOM
+//   document.getElementById('goals').innerHTML = ''
+//   document.getElementById('to-dos').innerHTML = ''
 
-  // Invoke DOM methods to append each of the state elements to DOM 
-  const { goals, todos } = store.getState()
-  goals.forEach(addGoalToDOM)
-  todos.forEach(addToDoToDOM)
-})
-
-// const unsubscribe = store.subscribe(() => {
-//   console.log('The store changed.')
+//   // Invoke DOM methods to append each of the state elements to DOM 
+//   const { goals, todos } = store.getState()
+//   goals.forEach(addGoalToDOM)
+//   todos.forEach(addToDoToDOM)
 // })
 
-// unsubscribe()
+// // const unsubscribe = store.subscribe(() => {
+// //   console.log('The store changed.')
+// // })
 
-// DOM Code
-function addToDo () {
-  const input = document.getElementById('to-do')
-  const name = input.value
-  input.value = ''
+// // unsubscribe()
 
-  store.dispatch(addToDoAction({
-    name,
-    complete: false,
-    id: generateId()
-  }))
-}
+// // DOM Code
+// function addToDo () {
+//   const input = document.getElementById('to-do')
+//   const name = input.value
+//   input.value = ''
 
-function addGoal () {
-  const input = document.getElementById('goal')
-  const name = input.value
-  input.value = ''
+//   store.dispatch(addToDoAction({
+//     name,
+//     complete: false,
+//     id: generateId()
+//   }))
+// }
 
-  store.dispatch(addGoalAction({
-    name,
-    id: generateId()
-  }))
-}
+// function addGoal () {
+//   const input = document.getElementById('goal')
+//   const name = input.value
+//   input.value = ''
 
-document.getElementById('toDoBtn')
-  .addEventListener('click', addToDo);
+//   store.dispatch(addGoalAction({
+//     name,
+//     id: generateId()
+//   }))
+// }
 
-document.getElementById('goalBtn')
-  .addEventListener('click', addGoal);
+// document.getElementById('toDoBtn')
+//   .addEventListener('click', addToDo);
 
-// Declare a function whose callback function is hooked to a DOM event listener
-function createRemoveButton (callback) {
-  const removeBtn = document.createElement('button')
-  removeBtn.innerHTML = 'X'
-  removeBtn.addEventListener('click', callback)
-  return removeBtn
-}
+// document.getElementById('goalBtn')
+//   .addEventListener('click', addGoal);
 
-function addToDoToDOM (todo) {
-  const node = document.createElement('li')
-  const text = document.createTextNode(todo.name)
+// // Declare a function whose callback function is hooked to a DOM event listener
+// function createRemoveButton (callback) {
+//   const removeBtn = document.createElement('button')
+//   removeBtn.innerHTML = 'X'
+//   removeBtn.addEventListener('click', callback)
+//   return removeBtn
+// }
 
-  const removeBtn = createRemoveButton(() => {
-    // The callback function that is hooked to createRemoveButton
-    // Invoke dispatch method
-    store.dispatch(removeToDoAction(todo.id))
-  })
+// function addToDoToDOM (todo) {
+//   const node = document.createElement('li')
+//   const text = document.createTextNode(todo.name)
 
-  node.appendChild(text)
-  node.appendChild(removeBtn)
+//   const removeBtn = createRemoveButton(() => {
+//     // The callback function that is hooked to createRemoveButton
+//     // Invoke dispatch method
+//     store.dispatch(removeToDoAction(todo.id))
+//   })
 
-  // Toggle complete boolean
-  node.style.textDecoration = todo.complete ? 'line-through' : 'none'
-  node.addEventListener('click', () => {
-    store.dispatch(toggleToDoAction(todo.id))
-  })
+//   node.appendChild(text)
+//   node.appendChild(removeBtn)
 
-  document.getElementById('to-dos')
-    .appendChild(node)
+//   // Toggle complete boolean
+//   node.style.textDecoration = todo.complete ? 'line-through' : 'none'
+//   node.addEventListener('click', () => {
+//     store.dispatch(toggleToDoAction(todo.id))
+//   })
 
-}
+//   document.getElementById('to-dos')
+//     .appendChild(node)
 
-function addGoalToDOM (goal) {
-  const node = document.createElement('li')
-  const text = document.createTextNode(goal.name)
+// }
 
-  // Purposely did not use _createRemoveButton_ method delcared above to demonstrate the callback invocation parameter in _addEventListener_ method
-  const removeBtn = document.createElement('button')
-  removeBtn.innerHTML = 'X'
-  removeBtn.addEventListener('click', () => {
-    // Invoke dispatch method
-    store.dispatch(removeGoalAction(goal.id))
-  })
+// function addGoalToDOM (goal) {
+//   const node = document.createElement('li')
+//   const text = document.createTextNode(goal.name)
 
-  node.appendChild(text)
-  node.appendChild(removeBtn)
+//   // Purposely did not use _createRemoveButton_ method delcared above to demonstrate the callback invocation parameter in _addEventListener_ method
+//   const removeBtn = document.createElement('button')
+//   removeBtn.innerHTML = 'X'
+//   removeBtn.addEventListener('click', () => {
+//     // Invoke dispatch method
+//     store.dispatch(removeGoalAction(goal.id))
+//   })
 
-  document.getElementById('goals')
-    .appendChild(node)
-}
+//   node.appendChild(text)
+//   node.appendChild(removeBtn)
+
+//   document.getElementById('goals')
+//     .appendChild(node)
+// }
